8812336a8f361cc04a07a0a240c75e375288d60c
diamouhamed <dia.mouhamed@outlook.fr>
2022-04-18 02:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,16 +23,19 @@ def get_categories(url):
             
             categs_dict = {"name" : name, "link" : link}
             categories.append(categs_dict)
-            
+    
+    del categories[0]
     return categories
  
 def main():
     url = "https://books.toscrape.com/"
+    
+    categories_url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+    
     categories = get_categories(url)
-        
-    for category in categories:
-        print(category)
+    
+    for x in categories:
+        print(x["link"])
     
 if __name__ == '__main__':
     main()
-    
