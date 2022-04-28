804e81f2aca496bad265d8e4804db4cff7b0e784
diamouhamed <dia.mouhamed@outlook.fr>
2022-04-28 18:39:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from pathlib import Path
 
 def get_categories(url):
     """return categories names & links"""
@@ -27,17 +28,16 @@ def get_categories(url):
 
 def get_books_urls(category_url):
     """return les urls des livres d'une catégorie"""
-    books_url = []
-    
     response = requests.get(category_url)
     soup = BeautifulSoup(response.content, "html.parser")
-    
+    books_url = []
     if response.ok:
         h3s = soup.find_all("h3")
         for h3 in h3s:
             a = h3.find('a')
             link = a['href']
             books_url.append(category_url.replace("/category/books","") + link)
+    
     return books_url
 
 def get_book_data(url):
@@ -45,10 +45,13 @@ def get_book_data(url):
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    
     #Récupération Product Page
     product_page = url
     #Récupération Title
     title = soup.find("h1").text
+    #Récupération UPC
+    upc = soup.find('th', string='UPC').find_next_sibling('td').string
     #Récupération Price Including Tax
     price_including_tax = soup.find("th", text="Price (incl. tax)").find_next_sibling("td").string
     #Récupération Price Encluding Tax
@@ -82,13 +85,16 @@ def get_book_data(url):
         review_rating = 0
     #Récupération Category
     category = soup.find("li").find_next_sibling("li").find_next_sibling("li").text.strip()
+    #Récupération Image Url
+    image_url = soup.find("div", {"id": "product_gallery"})
+    image_url = "http://books.toscrape.com/" + soup.find('img')["src"]
     #Récupération Product Description
     product_description = soup.find("div", id="product_description")
     if product_description:
         product_description = soup.find("div", id="product_description").find_next_sibling("p").text
     
     #Creation d'un dictionnaire avec toutes les informations des livres    
-    book_data = {"title": title, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax,"availability": availability, "review_rating": review_rating, "category":category, "product_description": product_description, "product_page": product_page} 
+    book_data = {"title": title,"upc": upc, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax,"availability": availability, "review_rating": review_rating, "category": category, "product_description": product_description,"image_url": image_url, "product_page": product_page} 
     
     return book_data
 
@@ -96,6 +102,9 @@ def main():
     """Fonction principale"""
     url = "https://books.toscrape.com/"
     categories = get_categories(url)
+    
+    DATA_CSV_DIR = "book_data/csv"
+    Path(DATA_CSV_DIR).mkdir(parents=True, exist_ok=True)
     
     for category in categories:
         books_urls_category = get_books_urls(category["link"])
@@ -107,11 +116,11 @@ def main():
             books_data.append(book_data)
         
         keys = books_data[0].keys()
-        with open('books_infos.csv', 'w', newline='', encoding="utf-8") as output_file:
+        with open(f"{DATA_CSV_DIR}/{category['name']}.csv", 'w', newline='', encoding="utf-8") as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(books_data)
-        return
+            return
 
 if __name__ == '__main__':
     main()
